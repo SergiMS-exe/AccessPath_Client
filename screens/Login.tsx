02f5e17e1af3aab9ae9b710302e5fffce81e9df8
Sergio Maté Suárez <sergio@MacBook-Air-de-Sergio.local>
2023-05-18 00:31:16 +0200

    Personalizacion de proyecto y primera vista hecha

---
+++ screens/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, SafeAreaView } from 'react-native';
+
+export const Login = () => {
+    return(
+        <SafeAreaView>
+              <Button title="Click Me" onPress={() => {}}/>
+        </SafeAreaView>
+    )
+}
